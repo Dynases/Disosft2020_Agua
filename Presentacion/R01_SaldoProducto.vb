425e1a9c2927b21eaae8bdf8378f80c22c24bebc
Carlos Fuentelsaz <carlosfu1102@gmail.com>
2021-01-14 11:37:57 -0400

    Commit Inicial

---
+++ Presentacion/R01_SaldoProducto.vb
@@ -0,0 +1,77 @@
+﻿Imports Logica.AccesoLogica
+Imports DevComponents.DotNetBar
+Imports DevComponents.DotNetBar.Controls
+Imports UTILITIES
+
+Public Class R01_SaldoProducto
+    Dim _inter As Integer = 0
+#Region "Variables Globales"
+
+    Public _nameButton As String
+    Public _tab As SuperTabItem
+    Public _modulo As SideNavItem
+
+#End Region
+
+#Region "Eventos"
+
+    Private Sub My_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        P_prInicio()
+    End Sub
+
+    Private Sub MBtGenerar_Click(sender As Object, e As EventArgs) Handles MBtGenerar.Click
+        P_prCargarReporte()
+    End Sub
+
+    Private Sub MBtSalir_Click(sender As Object, e As EventArgs) Handles MBtSalir.Click
+        Me.Close()
+        _modulo.Select()
+        '_tab.Close()
+    End Sub
+
+#End Region
+
+#Region "Metodos"
+
+    Private Sub P_prInicio()
+        'Abrir conexion dsds
+        If (Not gb_ConexionAbierta) Then
+            L_prAbrirConexion()
+        End If
+
+        Me.Text = "S A L D O   D E   P R O D U C T O".ToUpper
+        'Me.WindowState = FormWindowState.Maximized
+        MCrReporte.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
+
+    End Sub
+
+    Private Sub P_prCargarReporte()
+        Dim _dt As New DataTable
+        If swTipo.Value Then
+            Dim objrep As New R_StockActual()
+            _dt = L_VistaStockActual("cenum=0")
+            objrep.SetDataSource(_dt)
+            MCrReporte.ReportSource = objrep
+        Else
+            Dim objrep As New R_StockActualSinAgrupacion()
+            _dt = L_VistaStockActual("cenum=0")
+            objrep.SetDataSource(_dt)
+            MCrReporte.ReportSource = objrep
+        End If
+
+    End Sub
+
+#End Region
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        _inter = _inter + 1
+        If _inter = 1 Then
+            Me.WindowState = FormWindowState.Normal
+
+        Else
+            Me.Opacity = 100
+            Timer1.Enabled = False
+        End If
+        'Me.Opacity = 100
+        'Timer1.Enabled = False
+    End Sub
+End Class
---
+++ Presentacion/R01_StockDisponible.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class R01_StockDisponible
+    Inherits Modelo.ModeloR01
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelX5 = New DevComponents.DotNetBar.LabelX()
+        Me.swTipo = New DevComponents.DotNetBar.Controls.SwitchButton()
+        Me.MPnSuperior.SuspendLayout()
+        Me.MPnInferior.SuspendLayout()
+        Me.MPanelToolBarUsuario.SuspendLayout()
+        Me.MPanelToolBarAccion.SuspendLayout()
+        CType(Me.MBubbleBarUsuario, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MPnUsuario.SuspendLayout()
+        CType(Me.MEP, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MGpFiltro.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MPnSuperior
+        '
+        Me.MPnSuperior.Style.Alignment = System.Drawing.StringAlignment.Center
+        Me.MPnSuperior.Style.BackColor1.Color = System.Drawing.Color.Gray
+        Me.MPnSuperior.Style.BackColor2.Color = System.Drawing.SystemColors.ActiveCaption
+        Me.MPnSuperior.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine
+        Me.MPnSuperior.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder
+        Me.MPnSuperior.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText
+        Me.MPnSuperior.Style.GradientAngle = 90
+        '
+        'MPnInferior
+        '
+        Me.MPnInferior.Location = New System.Drawing.Point(300, 525)
+        Me.MPnInferior.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.MPnInferior.Size = New System.Drawing.Size(886, 36)
+        Me.MPnInferior.Style.Alignment = System.Drawing.StringAlignment.Center
+        Me.MPnInferior.Style.BackColor1.Color = System.Drawing.Color.Gray
+        Me.MPnInferior.Style.BackColor2.Color = System.Drawing.SystemColors.ActiveCaption
+        Me.MPnInferior.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine
+        Me.MPnInferior.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder
+        Me.MPnInferior.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText
+        Me.MPnInferior.Style.GradientAngle = 90
+        '
+        'MPanelToolBarUsuario
+        '
+        Me.MPanelToolBarUsuario.Location = New System.Drawing.Point(686, 0)
+        Me.MPanelToolBarUsuario.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        '
+        'MTbUsuario
+        '
+        Me.MTbUsuario.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MTbUsuario.ReadOnly = True
+        Me.MTbUsuario.Size = New System.Drawing.Size(135, 32)
+        Me.MTbUsuario.Text = "DEFAULT"
+        '
+        'MBtSalir
+        '
+        '
+        'MBtGenerar
+        '
+        '
+        'MBubbleBarUsuario
+        '
+        '
+        '
+        '
+        Me.MBubbleBarUsuario.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        '
+        '
+        '
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.BackColor = System.Drawing.Color.Transparent
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.BorderBottomWidth = 1
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.BorderColor = System.Drawing.Color.FromArgb(CType(CType(180, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer))
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.BorderLeftWidth = 1
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.BorderRightWidth = 1
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.BorderTopWidth = 1
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.PaddingBottom = 3
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.PaddingLeft = 3
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.PaddingRight = 3
+        Me.MBubbleBarUsuario.ButtonBackAreaStyle.PaddingTop = 3
+        Me.MBubbleBarUsuario.MouseOverTabColors.BorderColor = System.Drawing.SystemColors.Highlight
+        Me.MBubbleBarUsuario.SelectedTabColors.BorderColor = System.Drawing.Color.Black
+        '
+        'MGpFiltro
+        '
+        Me.MGpFiltro.Controls.Add(Me.LabelX5)
+        Me.MGpFiltro.Controls.Add(Me.swTipo)
+        Me.MGpFiltro.Controls.Add(Me.Label1)
+        Me.MGpFiltro.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.MGpFiltro.Size = New System.Drawing.Size(300, 491)
+        '
+        '
+        '
+        Me.MGpFiltro.Style.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.MGpFiltro.Style.BackColor2 = System.Drawing.Color.WhiteSmoke
+        Me.MGpFiltro.Style.BackColorGradientAngle = 90
+        Me.MGpFiltro.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.MGpFiltro.Style.BorderBottomWidth = 1
+        Me.MGpFiltro.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder
+        Me.MGpFiltro.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.MGpFiltro.Style.BorderLeftWidth = 1
+        Me.MGpFiltro.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.MGpFiltro.Style.BorderRightWidth = 1
+        Me.MGpFiltro.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.MGpFiltro.Style.BorderTopWidth = 1
+        Me.MGpFiltro.Style.CornerDiameter = 4
+        Me.MGpFiltro.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded
+        Me.MGpFiltro.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center
+        Me.MGpFiltro.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText
+        Me.MGpFiltro.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near
+        '
+        '
+        '
+        Me.MGpFiltro.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        '
+        '
+        '
+        Me.MGpFiltro.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.MGpFiltro.Controls.SetChildIndex(Me.Label1, 0)
+        Me.MGpFiltro.Controls.SetChildIndex(Me.swTipo, 0)
+        Me.MGpFiltro.Controls.SetChildIndex(Me.LabelX5, 0)
+        '
+        'MCrReporte
+        '
+        Me.MCrReporte.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.MCrReporte.Size = New System.Drawing.Size(886, 561)
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 65)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 15)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Stock > 0"
+        '
+        'LabelX5
+        '
+        Me.LabelX5.BackColor = System.Drawing.Color.Transparent
+        '
+        '
+        '
+        Me.LabelX5.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX5.Font = New System.Drawing.Font("Georgia", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelX5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.LabelX5.Location = New System.Drawing.Point(30, 28)
+        Me.LabelX5.Name = "LabelX5"
+        Me.LabelX5.SingleLineColor = System.Drawing.SystemColors.Control
+        Me.LabelX5.Size = New System.Drawing.Size(82, 23)
+        Me.LabelX5.TabIndex = 378
+        Me.LabelX5.Text = "Agrupado"
+        '
+        'swTipo
+        '
+        '
+        '
+        '
+        Me.swTipo.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.swTipo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.swTipo.Location = New System.Drawing.Point(118, 28)
+        Me.swTipo.Name = "swTipo"
+        Me.swTipo.OffBackColor = System.Drawing.Color.LawnGreen
+        Me.swTipo.OffText = "NO"
+        Me.swTipo.OnBackColor = System.Drawing.Color.Gold
+        Me.swTipo.OnText = "SI"
+        Me.swTipo.Size = New System.Drawing.Size(128, 22)
+        Me.swTipo.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.swTipo.TabIndex = 377
+        Me.swTipo.Value = True
+        Me.swTipo.ValueObject = "Y"
+        '
+        'R01_StockDisponible
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1186, 561)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "R01_StockDisponible"
+        Me.Opacity = 0.05R
+        Me.Text = "R01_SaldoProducto"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.Controls.SetChildIndex(Me.MPnInferior, 0)
+        Me.Controls.SetChildIndex(Me.MPnUsuario, 0)
+        Me.MPnSuperior.ResumeLayout(False)
+        Me.MPnInferior.ResumeLayout(False)
+        Me.MPanelToolBarUsuario.ResumeLayout(False)
+        Me.MPanelToolBarUsuario.PerformLayout()
+        Me.MPanelToolBarAccion.ResumeLayout(False)
+        CType(Me.MBubbleBarUsuario, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MPnUsuario.ResumeLayout(False)
+        Me.MPnUsuario.PerformLayout()
+        CType(Me.MEP, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MGpFiltro.ResumeLayout(False)
+        Me.MGpFiltro.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelX5 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents swTipo As DevComponents.DotNetBar.Controls.SwitchButton
+End Class
